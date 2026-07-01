7f10e5aa42551f90b8c94ac136afbb2760f4876e
techdev-prasanth <prokdjangoid@gmail.com>
2026-07-01 16:56:30 +0530

    rag with memor

---
+++ gaurdrails.py
@@ -0,0 +1,11 @@
+
+def allowed_mail(query):
+
+    ALLOWED_LITS = [
+    "hr",
+    "support",
+    "helpdesk",
+    ]
+
+
+    
--- ingest.py
+++ ingest.py
@@ -1,35 +1,34 @@
-from langchain_community.document_loaders  import PyPDFLoader
+from langchain_community.document_loaders  import PyPDFLoader,PyMuPDFLoader
 from dotenv import load_dotenv
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_qdrant import QdrantVectorStore, RetrievalMode
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 load = load_dotenv(override=True)
+import time
 
-FILE_PATH  = "D:\\ai_learning\\ai_projects\\hrpolicyrag\\HRPolicy.pdf"
+FILE_PATH  = "D:\\ai_learning\\ai_projects\\hrpolicyrag\\CompanyKnowledge.pdf"
 
 def knowledge_ingest(args=None,**kwargs):
-    loader = PyPDFLoader(FILE_PATH)
+    loader = PyMuPDFLoader(FILE_PATH)
 
     try:
-        documents  = loader.load()
         text_splitter  = RecursiveCharacterTextSplitter(
             chunk_size=500,
-            chunk_overlap=100,
+            chunk_overlap=50,
             length_function=len,
             is_separator_regex=False
 
         )
-
+        documents  = loader.load()
         chunks = text_splitter.split_documents(documents)
         embeddings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-2-preview")
-        # embeddings = GoogleGenerativeAIEmbeddings(model="text-embedding-004")
-        vector = QdrantVectorStore.from_documents(
+        QdrantVectorStore.from_documents(
                 chunks,
                 embeddings,
                 url=os.getenv("QDRANT_URL"),
                 api_key=os.getenv("QDRANT_API_KEY"),
-                collection_name="company_docs",
+                collection_name="company_knowledge_base",
                 retrieval_mode = RetrievalMode.DENSE,
                 prefer_grpc=True
             )
@@ -37,6 +36,7 @@ def knowledge_ingest(args=None,**kwargs):
 
         print("------------------")
         print("data successfully ingested",len(chunks))
+
     except Exception as e: 
         print("------------------")
         print(f"Ingestion Failed Due to {e}")
